fef43681fe85b99438330218405764dcfc01c939
Valerius Mattfeld <mail@valerius.me>
2022-06-11 14:57:04 +0200

    update e2e tests

--- cypress/e2e/02_get_to_the_first_attention_check.ts
+++ cypress/e2e/02_get_to_the_first_attention_check.ts
@@ -1,40 +1,27 @@
 describe("Click through the forms until the first attention check at 15 is passed", () => {
   it("should register a user and fill out 15 forms", () => {
-    cy.visit("http://localhost:3000/");
+    cy.visit("https://c109-055.cloud.gwdg.de/");
     cy.setCookie(
       "dfg_session",
-      "eyJ1c2VySWQiOiJjbDJzd25wMmQxMjI5MzZrbHNoMDVmZG9mc3oifQ%3D%3D.5Qr2DcTl4xKpKpklqw8A4jhijoWsuvZnxxmGikt8YPk"
+      "eyJ1c2VySWQiOiJjbDJzd3dmdm4wMDA2ZTd4NTk3dHMxdm9pIn0%3D.wUEg1lM%2FT%2FrB5FBJfFUSuh4QcRf3ylVKCw%2B0QuyGmP0"
     );
-    cy.visit("http://localhost:3000/survey/3");
+    cy.visit("https://c109-055.cloud.gwdg.de/survey/3");
     // cy.get("label").click({ multiple: true });
     // cy.get("button[type=submit]").click();
     cy.visit(
-      "http://localhost:3000/images/65ea1cfc-938c-4c5b-b986-3db344ecac89/qone"
+      "https://c109-055.cloud.gwdg.de/images/65ea1cfc-938c-4c5b-b986-3db344ecac89/qone"
     );
 
     for (let i = 0; i < 65; i++) {
-      cy.wait(500);
-
-      //   if (i == 16 || i == 30 || i == 45) {
-      //     cy.url().should("include", "att");
-      //     cy.get("[name=questionOne]").click();
-      //     cy.get("[name=confidenceOne]").click();
-      //     cy.get("button[type=submit]").click();
-      //     cy.wait(250);
-      //     continue;
-      //   } else {
-      //     cy.url().should("include", "qone");
-      //   }
-
+      cy.wait(1000);
       cy.get("label").contains("nicht entscheidbar").click();
       cy.get("label").contains("1 - sehr unsicher").click();
       cy.get("button[type=submit]").click();
-      cy.wait(250);
-      //   cy.url().should("include", "qtwo");
+      cy.wait(1000);
       cy.get("[type=checkbox]").first().check();
       cy.get("[type=radio]").first().check();
       cy.get("button[type=submit]").click();
-      cy.wait(500);
+      cy.wait(1000);
     }
   });
 });
